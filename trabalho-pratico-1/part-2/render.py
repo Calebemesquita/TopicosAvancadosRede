a28024e8b931b78fc2cf46bdea206956bf208ca3
Calebemesquita <calebemesquita.13@gmail.com>
2026-03-18 15:19:35 -0300

    gitignore

---
+++ trabalho-pratico-1/part-2/render.py
@@ -0,0 +1,20 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+with open('vars.yaml', 'r') as arquivo_dados:
+    dados = yaml.safe_load(arquivo_dados)
+
+ambiente = Environment(loader=FileSystemLoader('.'))
+template = ambiente.get_template('bgp.j2')
+
+for roteador in dados['roteadores']:
+    nome_arquivo = f"{roteador['hostname']}.conf"
+    config_gerada = template.render(roteador)
+    with open(nome_arquivo, 'w') as arquivo_saida:
+        arquivo_saida.write(config_gerada)
+        
+    print(f"Arquivo {nome_arquivo} gerado")
+
+
+print("Configurações fram um sucesso")
