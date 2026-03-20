a8462abe4390cc1ecb7b683a252819f2eeb2efcb
Calebemesquita <calebemesquita.13@gmail.com>
2026-03-20 13:13:35 -0300

    teste topologia pipeline

--- trabalho-pratico-1/part-4/topologia.py
+++ trabalho-pratico-1/part-4/topologia.py
@@ -2,82 +2,89 @@
 import json
 import os
 from mininet.net import Mininet
-from mininet.node import Controller, OVSKernelSwitch
+from mininet.node import OVSBridge
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from jinja2 import Environment, FileSystemLoader
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def gerar_configs_jinja():
-    info("***  Lendo o JSON do NetBox e Gerando Configs com Jinja2\n")
-    
-    with open('netbox_dados_jinja.json', 'r') as f:
+    info("*** [1] Lendo o JSON do NetBox e Gerando Configs com Jinja2...\n")
+    json_path = os.path.join(BASE_DIR, 'netbox_dados_jinja.json')
+    with open(json_path, 'r') as f:
         dados = json.load(f)
 
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template('bgpd.j2') 
+    env = Environment(loader=FileSystemLoader(BASE_DIR))
+    template = env.get_template('bgp.j2')
 
     for roteador in dados['roteadores']:
         hostname = roteador['hostname']
         config_renderizada = template.render(roteador)
+        dir_roteador = os.path.join(BASE_DIR, 'configs', hostname)
+        os.makedirs(dir_roteador, exist_ok=True)
         
-        
-        caminho_arquivo = f"configs/{hostname}/bgpd.conf"
+        caminho_arquivo = os.path.join(dir_roteador, 'bgpd.conf')
         with open(caminho_arquivo, 'w') as f:
             f.write(config_renderizada)
         
-        os.system(f"touch configs/{hostname}/zebra.conf")
-        os.system(f"chown mininet:mininet configs/{hostname}/* 2>/dev/null") 
-        
-        info(f"    - Configuração gerada para {hostname.upper()}\n")
+        info(f"    - Configuração gerada: {caminho_arquivo}\n")
 
 def iniciar_topologia():
-    info("***  Iniciando o Mininet\n")
-    net = Mininet(controller=Controller, switch=OVSKernelSwitch)
+    info("*** [2] Iniciando o Mininet...\n")
+    net = Mininet(controller=None, switch=OVSBridge)
 
-    info("*** Adicionando o Switch S1 \n")
+    info("*** Adicionando o Switch S1...\n")
     s1 = net.addSwitch('s1')
 
-    info("*** Adicionando os Roteadores e IPs...\n")
-    r1 = net.addHost('r1', ip='10.0.0.1/24')
-    r2 = net.addHost('r2', ip='10.0.0.2/24')
-    r3 = net.addHost('r3', ip='10.0.0.3/24')
-    r4 = net.addHost('r4', ip='10.0.0.4/24')
+    info("*** Adicionando os Roteadores com Pastas Privadas (Isolamento)...\n")
+    diretorios_privados = ['/etc/frr', '/var/run/frr']
+    
+    r1 = net.addHost('r1', ip='10.0.0.1/24', privateDirs=diretorios_privados)
+    r2 = net.addHost('r2', ip='10.0.0.2/24', privateDirs=diretorios_privados)
+    r3 = net.addHost('r3', ip='10.0.0.3/24', privateDirs=diretorios_privados)
+    r4 = net.addHost('r4', ip='10.0.0.4/24', privateDirs=diretorios_privados)
 
-    info("*** Passando os Cabos (Links)\n")
+    info("*** Passando os Cabos (Links)...\n")
     net.addLink(r1, s1)
     net.addLink(r2, s1)
     net.addLink(r3, s1)
     net.addLink(r4, s1)
 
-    info("*** Ligando a energia da Rede\n")
+    info("*** [3] Ligando a energia da Rede...\n")
     net.start()
 
-    info("*** Iniciando os Daemons do FRRouting (BGP)\n")
+    info("*** [4] Iniciando os Daemons do FRRouting (BGP)...\n")
     roteadores = [r1, r2, r3, r4]
     for r in roteadores:
         hostname = r.name
+        config_dir = os.path.join(BASE_DIR, 'configs', hostname)
         
         r.cmd('sysctl -w net.ipv4.ip_forward=1')
         
-        dir_atual = os.getcwd()
-        config_dir = f"{dir_atual}/configs/{hostname}"
-        
-        r.cmd(f'/usr/lib/frr/zebra -f {config_dir}/zebra.conf -d -i /tmp/zebra-{hostname}.pid')
-        
-        r.cmd(f'/usr/lib/frr/bgpd -f {config_dir}/bgpd.conf -d -i /tmp/bgpd-{hostname}.pid')
+        num = hostname.replace('r', '')
+        r.cmd(f'ip addr add 192.168.{num}.1/24 dev lo')
+        r.cmd('ip link set lo up')
+        r.cmd(f'cp {config_dir}/bgpd.conf /etc/frr/bgpd.conf')
+        r.cmd('touch /etc/frr/zebra.conf') 
+        r.cmd('touch /etc/frr/vtysh.conf')
+        r.cmd('chown -R frr:frrvty /etc/frr')
+        r.cmd('chown -R frr:frrvty /var/run/frr')
+        r.cmd('chmod 775 /var/run/frr')
+        r.cmd('/usr/lib/frr/zebra -f /etc/frr/zebra.conf -d -z /var/run/frr/zserv.api -i /var/run/frr/zebra.pid')
+        r.cmd('sleep 1')
+        r.cmd('/usr/lib/frr/bgpd -f /etc/frr/bgpd.conf -d -z /var/run/frr/zserv.api -i /var/run/frr/bgpd.pid')
 
-    info("*** Abrindo Terminal do Mininet \n")
+    info("*** [5] Rede BGP 100% Operacional! Abrindo Terminal do Mininet...\n")
     CLI(net)
 
-    info("*** Desligando tudo e limpando \n")
+    info("*** [6] Desligando tudo e limpando...\n")
     for r in roteadores:
-        r.cmd(f'kill `cat /tmp/bgpd-{r.name}.pid` 2>/dev/null')
-        r.cmd(f'kill `cat /tmp/zebra-{r.name}.pid` 2>/dev/null')
+        r.cmd('kill `cat /var/run/frr/bgpd.pid` 2>/dev/null')
+        r.cmd('kill `cat /var/run/frr/zebra.pid` 2>/dev/null')
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
-    
-    gerar_configs_jinja()
-    
+    gerar_configs_jinja() 
     iniciar_topologia()
