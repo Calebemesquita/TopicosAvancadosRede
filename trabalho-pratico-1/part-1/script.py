d138a1b5909c8fe8d9ad10b7d5bb58e44bb66d2a
Calebemesquita <calebemesquita.13@gmail.com>
2026-03-30 15:35:38 -0300

    versao final

--- trabalho-pratico-1/part-1/script.py
+++ trabalho-pratico-1/part-1/script.py
@@ -1,5 +1,3 @@
 import sys
-
 print("bateu aqui no script.py")
-
 sys.exit(0)
--- trabalho-pratico-1/part-4/treinando/carrefa_dados2.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('netbox_dados_jinja.json', 'r') as arquivo
-    dados = json.load(arquivo)
-
-
-roteadores = 
